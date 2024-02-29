d450f91842531e6ddd8218e0eea48590b0d37e6f
GökalpDemir <gokalpdmmr@gmail.com>
2024-02-29 15:04:20 +0300

    Refactor

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -12,7 +12,7 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-            classAttributes.Add(new PerformanceAspect(1));
+            classAttributes.Add(new PerformanceAspect(5));
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
     }
